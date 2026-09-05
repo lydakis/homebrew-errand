aad903508f16911ae04d7dd2a9027a31c346a6e6
George Lydakis <george@lydakis.me>
2026-09-05 13:44:45 +0000

    Update errand to 0.1.0

---
+++ Formula/errand.rb
@@ -0,0 +1,26 @@
+class Errand < Formula
+  desc "Personal job runner for machines you own"
+  homepage "https://github.com/lydakis/errand"
+  url "https://github.com/lydakis/errand/releases/download/v0.1.0/errand_0.1.0_source.tar.gz"
+  sha256 "96414d0fdeff46c44b9147e4bf89f70da0faa6a68d443efa3717db3da7f6cd9b"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/errand"
+  end
+
+  def caveats
+    <<~EOS
+      On a runner, use `errand setup` to configure and manage the service.
+      After upgrading, run `errand setup` when the runner is idle to restart it.
+    EOS
+  end
+
+  test do
+    assert_equal "errand #{version}", shell_output("#{bin}/errand version").strip
+    assert_match "errand peers", shell_output("#{bin}/errand --help")
+  end
+end
