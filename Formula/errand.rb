8c106456b05d16a425a546d9a18f3c88ac0279a1
George Lydakis <george@lydakis.me>
2026-09-12 16:26:46 +0000

    Update errand to 0.4.2

--- Formula/errand.rb
+++ Formula/errand.rb
@@ -1,8 +1,8 @@
 class Errand < Formula
   desc "Personal job runner for machines you own"
   homepage "https://github.com/lydakis/errand"
-  url "https://github.com/lydakis/errand/releases/download/v0.4.1/errand_0.4.1_source.tar.gz"
-  sha256 "83854774be6285bdff0b91018a772e25afe9b994b57ba3fab164dc33f39204f8"
+  url "https://github.com/lydakis/errand/releases/download/v0.4.2/errand_0.4.2_source.tar.gz"
+  sha256 "d98eb7b5619d1d663c32db752cfc1de816d00359c3a2ab5e7d0e242ee98eba7c"
   license "MIT"
 
   depends_on "go" => :build
